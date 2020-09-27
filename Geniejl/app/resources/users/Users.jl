ba43b06e9b111e4521b4d6e199d6aaaa22a5b2fc
Diaga <diagaaos@gmail.com>
2020-09-28 04:24:37 +0500

    Assign kw definitions

--- Geniejl/app/resources/users/Users.jl
+++ Geniejl/app/resources/users/Users.jl
@@ -7,10 +7,10 @@ export User
 
 @kwdef mutable struct User <: AbstractModel
   id::DbId = DbId()
-  discordId::Int64
-  accessToken::String
-  refreshToken::String
-  expiresIn::Int64
+  discordId::Int64 = 0
+  accessToken::String = ""
+  refreshToken::String = ""
+  expiresIn::Int64 = 0
 end
 
 end
