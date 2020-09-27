643e1eac9a244f0c0b4e937fc2c52a14c2108a8c
Diaga <diagaaos@gmail.com>
2020-09-27 20:33:05 +0500

    Add initial zoom auth callabck

--- Geniejl/routes.jl
+++ Geniejl/routes.jl
@@ -1,5 +1,35 @@
 using Genie.Router
 
+using HTTP
+using Base64
+
+using SearchLight
+using SearchLightSQLite
+using Users
+
 route("/") do
   serve_static_file("welcome.html")
+end
+
+route("/auth/*") do 
+  code = haskey(@params, :code) ? @params(:code) : ""
+  discordId = haskey(@params, :state) ? @params(:state) : 0
+
+  user_check = findone(User; :discordId => discordId)
+  if length(user_check) != 0 do
+    return "User already exists!"
+  end
+
+  auth = base64encode("$(ENV["ZOOM_CLIENT_ID"]):$(ENV["ZOOM_CLIENT_SECRET"])")
+
+  req = HTTP.request("POST", "https://zoom.us/oauth/token?grant_type=authorization_code&code=$code&redirect_uri=https://zoomcord.ml", ("Authorization" => "Basic $auth"))
+
+  accessToken = r.body["access_token"]
+  refreshToken = r.body["refresh_token"]
+  expiresIn = r.body["expires_in"]
+
+  user = User(:discordId = discordId, :accessToken = accessToken, refreshToken = refreshToken, expiresIn = expiresIn)
+  user |> save!
+
+  serve_static_file("auth.html")
 end
