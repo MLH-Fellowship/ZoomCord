ff0865d23f38af73de169ed6918aa79c1ef9bf59
Diaga <diagaaos@gmail.com>
2020-09-28 03:18:22 +0500

    Update comparison to check if user already exists

--- Geniejl/routes.jl
+++ Geniejl/routes.jl
@@ -16,7 +16,7 @@ route("/auth/*") do
   discordId = haskey(@params, :state) ? @params(:state) : 0
 
   user_check = findone(User; :discordId => discordId)
-  if length(user_check) != 0
+  if user_check === nothing
     return "User already exists!"
   end
 
