c07ea73bddf3059658bb9035b93dba4e807c98bb
Diaga <diagaaos@gmail.com>
2020-09-29 00:47:13 +0500

    Complete authorization access to database

--- Geniejl/routes.jl
+++ Geniejl/routes.jl
@@ -21,17 +21,9 @@ route("/auth/*") do
 
   discordId = haskey(state, "discordId") ? state["discordId"] : 0
   channelId = haskey(state, "channelId") ? state["channelId"] : 0
-  messageId = haskey(state, "messageId") ? state["messageId"] : 0 
+  messageId = haskey(state, "messageId") ? state["messageId"] : 0
 
-  println(discordId)
-
-  responsetype = haskey(@params, :response_type) ? @params(:response_type) == "code" : false
-
-  if !responsetype
-    return "Already authorized"
-  end
-
-  user_check = findone(User; :discordId => discordId)
+  user_check = findone(Users.User; :discordId => discordId)
   if user_check !== nothing
     return "User already exists!"
   end
@@ -39,7 +31,7 @@ route("/auth/*") do
   auth = base64encode("$(ENV["ZOOM_CLIENT_ID"]):$(ENV["ZOOM_CLIENT_SECRET"])")
 
   res = HTTP.request("POST", "https://zoom.us/oauth/token?grant_type=authorization_code&code=$code&redirect_uri=https://zoomcord.ml/auth/"; 
-  headers= ["Authorization" => "Basic $auth"])
+  headers = ["Authorization" => "Basic $auth"])
 
   body = JSON.parse(String(res.body))
 
@@ -48,18 +40,15 @@ route("/auth/*") do
   expiresIn = body["expires_in"]
 
   user = Users.User(discordId = discordId, accessToken = accessToken, refreshToken = refreshToken, expiresIn = expiresIn)
-  println(user)
   user |> save!
 
-  editMessage = @task begin
-    c = Client(ENV["DISCORD_CLIENT_TOKEN"]; presence=(game=(name="with Discord.jl", type=AT_GAME), ))
-    open(c)
 
-    edit_message(c, channelId, messageId; content="Authorization successful!")
+  c = Client(ENV["DISCORD_CLIENT_TOKEN"]; presence=(game=(name="with Discord.jl", type=AT_GAME), ))
+  open(c)
 
-    close(c)
-  end
-  schedule(editMessage)
+  edit_message(c, channelId, messageId; content="Authorization successful!")
 
+  close(c)
+  
   serve_static_file("auth.html")
 end
